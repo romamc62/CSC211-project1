d632027c26677e6b5fec435957305f74a2de96c4
romamc62 <116588825+romamc62@users.noreply.github.com>
2022-12-08 09:52:38 -0500

    reupload properly

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
-module guzmjo.JanKenPon {
+module com.mycompany.gamehub_project {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
-    opens guzmjo.JanKenPon to javafx.fxml;
-    exports guzmjo.JanKenPon;
+    opens com.mycompany.gamehub_project to javafx.fxml;
+    exports com.mycompany.gamehub_project;
 }
