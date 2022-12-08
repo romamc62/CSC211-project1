866a1c69ffb625d5d48a2cc0e1905b9bf1718713
romamc62 <116588825+romamc62@users.noreply.github.com>
2022-12-08 09:52:38 -0500

    reupload properly

--- src/main/java/com/mycompany/gamehub_project/Login.java
+++ src/main/java/com/mycompany/gamehub_project/Login.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 package com.mycompany.gamehub_project;
 
 import java.io.IOException;
@@ -22,52 +20,4 @@ public class Login {
     private void switchToGameHub() throws IOException {
         App.setRoot("GameHub");
     }
-=======
-package com.mycompany.gamehub_project;
-
-import java.io.IOException;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-
-public class Login {
-
-    @FXML
-    private Button CreateButton;
-    @FXML
-    private Button GameHubButton;
-
-    @FXML
-    private void switchToCreate() throws IOException {
-        App.setRoot("Create");
-    }
-    
-    @FXML
-    private void switchToGameHub() throws IOException {
-        App.setRoot("GameHub");
-    }
->>>>>>> 38990ea11eb6ccec7b5557d2912962379a0e7abc
-=======
-package com.mycompany.gamehub_project;
-
-import java.io.IOException;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-
-public class Login {
-
-    @FXML
-    private Button CreateButton;
-    @FXML
-    private Button GameHubButton;
-
-    @FXML
-    private void switchToCreate() throws IOException {
-        App.setRoot("Create");
-    }
-    
-    @FXML
-    private void switchToGameHub() throws IOException {
-        App.setRoot("GameHub");
-    }
->>>>>>> 38990ea11eb6ccec7b5557d2912962379a0e7abc
 }
