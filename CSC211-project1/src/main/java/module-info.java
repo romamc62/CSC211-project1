a937f69c1a72edcfa2e920b45aea8ba2b0fcb075
romamc62 <116588825+romamc62@users.noreply.github.com>
2022-12-08 09:56:06 -0500

    Delete CSC211-project1 directory

--- CSC211-project1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.gamehub_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.mycompany.gamehub_project to javafx.fxml;
-    exports com.mycompany.gamehub_project;
-}
